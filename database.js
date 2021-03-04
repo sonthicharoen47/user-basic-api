fc970a5223da2cea7f1c03469ccba88a7ed4b6e1
sonthicharoen47 <sonthicharoen47@gmail.com>
2021-03-04 07:01:13 +0700

    changed database url

--- database.js
+++ database.js
@@ -1,12 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+// module.exports = new Sequelize(
+//   process.env.DATABASE,
+//   process.env.USER,
+//   process.env.PASS,
+//   {
+//     host: process.env.HOST,
+//     dialect: "postgres",
+//   }
+// );
+
 module.exports = new Sequelize(
-  process.env.DATABASE,
-  process.env.USER,
-  process.env.PASS,
-  {
-    host: process.env.HOST,
-    dialect: "postgres",
-  }
+  "postgres://lnjzesrbjqgdai:677b061c26832f33f3cc852742ff3f04d6a1e6e69574777cf2a3a13d3c0f90a7@ec2-18-211-97-89.compute-1.amazonaws.com:5432/d672r1q0gvj3rp"
 );
