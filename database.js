6d7b94b94a477e3fae81d4e2ca5856fa526c2fc2
sonthicharoen47 <sonthicharoen47@gmail.com>
2021-03-04 07:06:40 +0700

    update database connect

--- database.js
+++ database.js
@@ -11,6 +11,8 @@ require("dotenv").config();
 //   }
 // );
 
-module.exports = new Sequelize(
-  "postgres://lnjzesrbjqgdai:677b061c26832f33f3cc852742ff3f04d6a1e6e69574777cf2a3a13d3c0f90a7@ec2-18-211-97-89.compute-1.amazonaws.com:5432/d672r1q0gvj3rp"
-);
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  host: "localhost",
+  port: 5432,
+});
