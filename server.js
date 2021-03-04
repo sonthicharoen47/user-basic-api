41a02f9085c7b2a0de3dae0986beff8daf5d014f
sonthicharoen47 <sonthicharoen47@gmail.com>
2021-03-04 07:11:42 +0700

    async func db con

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const db = require("./database");
-db.authenticate();
+async function dbCon() {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+dbCon();
 
 app.use(express.json());
 
